c6f1bc4e102d70661b4080bc6bda996a68d5e230
mathiscolbaut <mathiscolbaut@gmail.com>
2023-01-04 17:28:34 +0100

    maj
    17h28

--- portfolio/src/Controller/VeilleController.php
+++ portfolio/src/Controller/VeilleController.php
@@ -11,8 +11,13 @@ class VeilleController extends AbstractController
     #[Route('/veille', name: 'app_veille')]
     public function index(): Response
     {
+        $rss = simplexml_load_file('https://www.google.fr/alerts/feeds/14623178845856607709/7148342474963529294');
+      
+
+
         return $this->render('veille/index.html.twig', [
             'controller_name' => 'VeilleController',
+             'rssItems' => $rss->channel->item
         ]);
     }
 }
