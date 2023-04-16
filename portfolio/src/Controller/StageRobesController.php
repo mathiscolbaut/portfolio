a506a10430d561902c2c8ef9ea89de29363335b4
mathiscolbaut <mathiscolbaut@gmail.com>
2023-04-16 15:35:42 +0200

    Présentation de AMO

--- portfolio/src/Controller/StageRobesController.php
+++ portfolio/src/Controller/StageRobesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class StageRobesController extends AbstractController
 {
@@ -15,4 +16,5 @@ class StageRobesController extends AbstractController
             'controller_name' => 'StageRobesController',
         ]);
     }
+
 }
