ee9f66bf0d8f6b4c0ce53940bf75f38bfb738bf1
mathiscolbaut <mathiscolbaut@gmail.com>
2023-04-26 21:30:34 +0200

    Portfolio Tableau Synthèse

--- portfolio/src/Controller/VeilleController.php
+++ portfolio/src/Controller/VeilleController.php
@@ -11,13 +11,16 @@ class VeilleController extends AbstractController
     #[Route('/veille', name: 'app_veille')]
     public function index(): Response
     {
-        $rss = simplexml_load_file('https://www.google.fr/alerts/feeds/14623178845856607709/7148342474963529294');
-      
-
-
+        $rss = simplexml_load_file('https://www.google.com/alerts/feeds/14623178845856607709/14361946119755138195');
+        $rss2 = simplexml_load_file('https://www.google.com/alerts/feeds/14623178845856607709/3732636363033765166');
+        // Définir le namespace Atom
+        $atom = $rss->children('http://www.w3.org/2005/Atom');
+        $atom2 = $rss2->children('http://www.w3.org/2005/Atom');
+        
         return $this->render('veille/index.html.twig', [
             'controller_name' => 'VeilleController',
-             'rssItems' => $rss->channel->item
+            'rssItems' => $atom->entry,
+            'rssItems2' => $atom2->entry,
         ]);
     }
 }
